ba3c27e06b70ee1434d38a41970ca2b8bee66f11
Wellington Donato <dev.donatello3@gmail.com>
2024-01-24 09:31:49 -0300

    feat: create home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,50 @@
-import './style.css'
+import Head from 'next/head'
+import './style.css';
+import localImage from "../image/img_login.png";
+import Image from 'next/image';
+
 const Login = () => (
-    <h2 className='teste'>
-        Login
-    </h2>
+  <>  
+<Head>
+  <title>Login</title>
+  
+</Head>
+
+<main className='container'>
+<Image className='image' src={localImage}
+/>
+
+
+
+
+<section className='content'>
+  <h1 className='titulo'>Entre no Orange Portfólio</h1>
+  <h2 className='subtitulo'>Faça login com email</h2>  
+
+  <input className='botoeslogin'  type='email' placeholder=' Camila.ux@gmail.com'></input>
+
+  <input className='botoeslogin'  type='password'></input>
+
+
+  <button className='entrar' type='submit'>Entrar</button>
+
+  <a className='link_cadastro' href=''>Cadastre-se</a> 
+
+  </section>
+
+
+ 
+
+ </main> 
+ 
+ 
+ 
+ 
+ 
+</>
+
+
+  
+  
 )
 export default Login
